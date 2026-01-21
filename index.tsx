806e9ff2bd07f4c009240a7284ad576fa59be6a8
tomas <tomasnavarrox@gmail.com>
2026-01-21 16:01:04 +0100

    Add version check log

--- index.tsx
+++ index.tsx
@@ -8,6 +8,8 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+console.log(`%c MEDICORE APP VERSION: ${new Date().toISOString()} `, 'background: #222; color: #bada55; padding: 4px; border-radius: 4px;');
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
